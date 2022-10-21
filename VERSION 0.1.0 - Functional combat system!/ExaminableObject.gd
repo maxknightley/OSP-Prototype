6a082aa2c6704e414c7bbe79e401c40affca890c
maxknightley <maxieknightley@gmail.com>
2022-10-20 19:15:56 -0500

    Starting to set up overworld navigation + dialogue system
    Note that the example battle WILL NOT show up by default; for that, go to Project Settings > Application > Run and change "TestLevel.tscn" to "ExampleBattle.tscn"

---
+++ VERSION 0.1.0 - Functional combat system!/ExaminableObject.gd
@@ -0,0 +1,27 @@
+extends Area2D
+
+signal sendTextToDisplay
+
+# Variable to determine whether the player can "speak" to/"read"/"examine" the object.
+# Turns on when the "leading character" is colliding with it, off otherwise.
+var active = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_pressed("ui_accept") && active:
+		emit_signal("sendTextToDisplay", 
+			["This is a sign!",
+			"By pressing 'Z', you've accessed some GENERIC TEST DIALOGUE.",
+			"I'm still working on getting it all to show up, though..."])
+
+func _on_Sign_body_entered(body):
+	$AnimatedSprite.play("highlighted")
+	active = true
+
+func _on_Sign_body_exited(body):
+	$AnimatedSprite.play("default")
+	active = false
---
+++ VERSION 0.1.0 - Functional combat system!/OW_Leader.gd
@@ -0,0 +1,25 @@
+extends Node2D
+
+# Velocity of character, used to handle movement across the stage.
+var velocity = Vector2.ZERO
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	
+	# Reset horizontal movement.
+	velocity.x = 0
+	
+	# Check if the character should be moving left or right.
+	if Input.is_action_pressed("move_left"): velocity.x -= 2.7
+	if Input.is_action_pressed("move_right"): velocity.x += 2.7
+		
+	# If the player is holding the "run" button, increase their velocity accordingly.
+	if Input.is_action_pressed("run"): velocity.x *= 2.4
+	
+	# Adjust the leader's position.
+	position += velocity
